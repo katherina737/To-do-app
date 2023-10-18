53022c23ed200d1434651e635721a36b55696c93
Katerina Silhanova <96746899+katherina737@users.noreply.github.com>
2023-10-18 15:15:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,3 @@
-// // when clicking Add butoton:
-// function addTask(){
-//     if (document.getElementById("write-task").value === ""){
-//         alert("You have to write something first.");
-//     }
-
-//     else{
-//         // new task text:
-//         var newTask = document.getElementById("list-text");
-//         var inputText = document.getElementById("write-task").value;
-        
-//         var newDiv = document.createElement("div");
-//         var newLi = document.createElement("li");
-
-//         newDiv.setAttribute("id","list-text");
-//         newLi.classList.add("list-box");
-        
-
-//         newLi.innerHTML = inputText;
-
-//         newTask.appendChild(newDiv);
-//         newTask.appendChild(newLi);
-//         // delete button:
-//         var del = document.createElement("BUTTON");
-//         del.classList.add("delete-button");
-//         del.innerHTML = '<img src="images/recycle-bin.png" width="20px">';
-//         del.style.marginLeft = "5px";
-//         del.onclick = deleteTask;
-
-//         newTask.appendChild(del);
-//     }
-//     document.getElementById("write-task").value = "";
-// }
-
-// // when clicking on delete button:
-// function deleteTask(){
-//     // alert("hello");
-//     this.parentNode.remove()
-//     // var currentTask = document.querySelectorAll(".delete-button");
-//     // for (var i=0; i<currentTask.length; i++) {
-//     //     // currentTask[i].parentNode.remove();
-//     //     currentTask[i] = 
-//     //         this.parentNode.remove();
-         
-//     // } 
-
-//     }
-
 document.querySelector(".add-button").onclick = function(){
     if(document.querySelector(".newTask input").value.length == 0){
         alert("Add task name!");
@@ -69,6 +21,15 @@ document.querySelector(".add-button").onclick = function(){
             currentTask[i].onclick = function(){
                 this.parentNode.remove();
             }    
+        }
+        var thisTask = document.querySelectorAll(".done");
+        for (var i=0; i<thisTask.length; i++) {
+            thisTask[i].onclick = function(){
+                // this.getElementById("taskname").value.write(strike());
+                this.style.background="green";
+                this.parentNode.classList.add("checked");
+            }    
+            
         }
     }
     document.getElementById("write-task").value = "";
