eeefb87c3f20a011339d3b04302077f4531ea282
Katerina Silhanova <96746899+katherina737@users.noreply.github.com>
2023-10-19 14:33:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
-<!DOCTYPE html>
-<html lang="en" >
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400&display=swap" rel="stylesheet">
-    <title>To Do List</title>
-</head>
-<body id="grad">
-    <div id="main-box"><br>To-Do List
-        <div class="newTask">
-            <br>
-            <input type="text" placeholder="Write something.." id="write-task">
-            <button  class="add-button">+</button>
+document.querySelector(".add-button").onclick = function(){
+    if(document.querySelector(".newTask input").value.length == 0){
+        alert("Add a task name!");
+    } else {
+        document.querySelector("#tasks").innerHTML +=
+        `<div class="task">
+            <span id="taskname">
+                ${document.querySelector(".newTask input").value}
+            </span>
+            <button class="delete">
+            <img src="images/remove.png" width="20px">
+            </button>
+            <button class="done">
+            <img src="images/done.png" width="20px">
+            
+            </button>
         </div>
-
-        <div id="tasks">
-        </div>
-        
-    </div>
-    <div class="my-name"><h4>by <a href="https://www.linkedin.com/in/katerina-silhanova-7b627a118/">Katerina Silhanova</a></h4></div>
-    <script src="index.js" charset="utf-8"></script>
-</body>
-</html>
+        `;
+        var currentTask = document.querySelectorAll(".delete");
+        for (var i=0; i<currentTask.length; i++) {
+            currentTask[i].onclick = function(){
+                this.parentNode.remove();
+            }    
+        }
+        var thisTask = document.querySelectorAll(".done");
+        for (var i=0; i<thisTask.length; i++) {
+            thisTask[i].onclick = function(){
+                this.style.background="#17594A";
+                this.parentNode.classList.add("checked");
+            }    
+            
+        }
+    }
+    document.getElementById("write-task").value = "";
+}
